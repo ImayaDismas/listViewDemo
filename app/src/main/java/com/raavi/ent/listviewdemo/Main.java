296c1a8a3d0a7e9a0c3d48d13f8f2c52d90f20c2
joseph muli <jayjaymuli00@gmail.com>
2016-03-06 16:35:16 +0300

    added listItemclick + toast

--- app/src/main/java/com/raavi/ent/listviewdemo/Main.java
+++ app/src/main/java/com/raavi/ent/listviewdemo/Main.java
@@ -3,7 +3,10 @@ package com.raavi.ent.listviewdemo;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -24,6 +27,13 @@ public class Main extends ListActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main);
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cars));
-
     }
+
+    public void onListItemClick(
+            ListView parent, View v, int position, long id)
+    {
+        Toast.makeText(this, "You have selected "+ cars[position], Toast.LENGTH_LONG).show();
+    }
+
+
 }
