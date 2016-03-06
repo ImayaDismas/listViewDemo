c70ab785ff9ef87960636c736a8bba87d41812cf
joseph muli <jayjaymuli00@gmail.com>
2016-03-06 16:23:24 +0300

    added files, changed main to ListACtivity

---
+++ app/src/main/java/com/raavi/ent/listviewdemo/Main.java
@@ -0,0 +1,16 @@
+package com.raavi.ent.listviewdemo;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class Main extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
