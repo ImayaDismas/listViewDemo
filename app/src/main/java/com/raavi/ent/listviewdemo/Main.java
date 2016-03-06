9687bfb66b3760f55880aef3af3dd52831c30d2b
joseph muli <jayjaymuli00@gmail.com>
2016-03-06 16:30:31 +0300

    added adapter

--- app/src/main/java/com/raavi/ent/listviewdemo/Main.java
+++ app/src/main/java/com/raavi/ent/listviewdemo/Main.java
@@ -3,14 +3,27 @@ package com.raavi.ent.listviewdemo;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.List;
 
 public class Main extends ListActivity {
 
+    String [] cars = {
+            "toyota",
+            "benz",
+            "ferrari",
+            "lamborghini",
+            "ford",
+            "land-rover",
+            "isuzu"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cars));
+
     }
 }
